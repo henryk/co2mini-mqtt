dc5c221d963fcce472d12c2696a22733262bf227
Henryk Plötz <henryk@ploetzli.ch>
2019-03-25 02:09:00 +0100

    Make it gracefully handle sensor reconnects (Well, by dying, needs systemd to keep running)

--- co2mini-mqtt.py
+++ co2mini-mqtt.py
@@ -1,4 +1,4 @@
-from CO2Meter import CO2Meter, CO2METER_CO2, CO2METER_HUM, CO2METER_TEMP
+from CO2Meter_mod import CO2Meter_mod as CO2Meter, CO2METER_CO2, CO2METER_HUM, CO2METER_TEMP
 from paho.mqtt.client import Client
 import platform, json, time, yaml
 
@@ -17,17 +17,23 @@ class Co2miniMqtt:
         self.sensor = None
         self.mqttc = None
         self.state = {}
+        self.disc_config = {}
+        self.alive = True
         self.last_pub = {}
+
+    def on_connect(self, *args, **kwargs):
+        self.mqttc.publish("{}/config".format(self.prefix), json.dumps(self.disc_config), retain=True)
+
+        self.mqttc.publish("{}/status".format(self.prefix), 'online', retain=True)
 
     def main(self):
         self.mqttc = Client()
         self.mqttc.will_set("{}/status".format(self.prefix), 'offline', retain=True)
         self.mqttc.enable_logger()
         self.mqttc.username_pw_set(self.config.get('mqtt', {}).get('username', 'homeassistant'), self.config.get('mqtt', {}).get('password', ''))
-        self.mqttc.connect(self.config.get('mqtt', {}).get('broker', '127.0.0.1'), port=self.config.get('mqtt', {}).get('port', 1883), keepalive=self.config.get('mqtt', {}).get('keepalive', 60))
-        self.mqttc.loop_start()
+        self.mqttc.on_connect = self.on_connect
 
-        disc_config = {
+        self.disc_config = {
             'state_topic': "{}/state".format(self.prefix),
             #'expire_after': 10,
             'name': self.config.get('name', '{} co2mini'.format(platform.node())),
@@ -42,16 +48,21 @@ class Co2miniMqtt:
             'device': {
                 'connections': [
                     ['usb', self.config.get('device', '/dev/co2mini0')],
-                ]
+                ],
+                'identifiers':
+                    ('dns', platform.node()),
+                'manufacturer': 'Various',
+                'model': 'co2mini',
             }
         }
 
-        self.mqttc.publish("{}/config".format(self.prefix), json.dumps(disc_config), retain=True)
-
         self.sensor = CO2Meter(self.config.get('device', '/dev/co2mini0'), self.sensor_callback)
-        self.mqttc.publish("{}/status".format(self.prefix), 'online', retain=True)
 
-        while True:
+        self.mqttc.connect(self.config.get('mqtt', {}).get('broker', '127.0.0.1'), port=self.config.get('mqtt', {}).get('port', 1883), keepalive=self.config.get('mqtt', {}).get('keepalive', 60))
+        self.mqttc.loop_start()
+
+        while self.alive:
+            self.alive = False
             time.sleep(10)
 
     def sensor_callback(self, sensor, value):
@@ -64,6 +75,7 @@ class Co2miniMqtt:
             if v != self.last_pub.get(k, None):
                self.mqttc.publish("{}/{}".format(self.prefix, k), v, retain=True)
                self.last_pub[k] = v
+        self.alive = True
 
 
 if __name__ == "__main__":
